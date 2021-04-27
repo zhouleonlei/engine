2470042c7d0a1903bb65f9d6e41a2100040ed4c9
Swift Kim <swift.kim@samsung.com>
2021-04-27 10:22:02 +0900

    Separate builds for profiles (#67)
    * Separate binaries
    
    * Clean up the build script
    
    * Use templates
    
    * Use __dlog_print() only on TV
    
    * Copy headers and icudata
    
    * Partially update azure-pipelines.yml
    
    * Temporarily unsupport Tizen 4.0 and use only ecore_wl2
    
    * Cherry-pick "Enable Evas GL direct mode (#54)"
    
    - Cherry-pick the commit from flutter-2.0.1-tizen-dev
    - Rename FLUTTER_TIZEN_EVASGL with TIZEN_RENDERER_EVAS_GL
    - Add missing switches and fix build errors
    
    Co-authored-by: Boram Bae <boram21.bae@samsung.com>
    Co-authored-by: MuHong Byun <mh.byun@samsung.com>
    
    * Fix CI build
    
    * Disable Evas GL direct mode and remove use of elm_win_aux_hint_add
    
    * Simplify the CI job
    
    * A workaround for isIME in Evas GL mode
    
    * Refactor: Clean up tizen_renderer.h and re-order functions
    
    * Refactor: Rename GetEcoreWindowId and change its return type to uintptr_t
    
    * Refactor: Make GetImageHandle() Evas GL-only
    
    * Refactor: Additional clean ups
    
    * Initialize members properly
    
    * Get window id from evas_window_
    
    * Re-format code
    
    Co-authored-by: Boram Bae <boram21.bae@samsung.com>
    Co-authored-by: MuHong Byun <mh.byun@samsung.com>

---
+++ shell/platform/tizen/tizen_renderer_evas_gl.h
@@ -0,0 +1,65 @@
+// Copyright 2021 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EMBEDDER_TIZEN_RENDERER_EVAS_GL_H
+#define EMBEDDER_TIZEN_RENDERER_EVAS_GL_H
+
+#undef EFL_BETA_API_SUPPORT
+#include <Ecore.h>
+#include <Elementary.h>
+#include <Evas_GL.h>
+
+#include "flutter/shell/platform/tizen/tizen_renderer.h"
+
+class TizenRendererEvasGL : public TizenRenderer {
+ public:
+  explicit TizenRendererEvasGL(TizenRenderer::Delegate& delegate, int32_t x,
+                               int32_t y, int32_t w, int32_t h);
+  virtual ~TizenRendererEvasGL();
+
+  bool OnMakeCurrent() override;
+  bool OnClearCurrent() override;
+  bool OnMakeResourceCurrent() override;
+  bool OnPresent() override;
+  uint32_t OnGetFBO() override;
+  void* OnProcResolver(const char* name) override;
+
+  TizenWindowGeometry GetGeometry() override;
+  uintptr_t GetWindowId() override;
+
+  void ResizeWithRotation(int32_t x, int32_t y, int32_t width, int32_t height,
+                          int32_t angle) override;
+  void SetRotate(int angle) override;
+
+  void* GetImageHandle();
+
+ private:
+  void ClearColor(float r, float g, float b, float a);
+
+  bool InitializeRenderer(int32_t x, int32_t y, int32_t w, int32_t h);
+  void Show();
+  void DestroyRenderer();
+
+  bool SetupEvasGL(int32_t x, int32_t y, int32_t w, int32_t h);
+  void* SetupEvasWindow(int32_t x, int32_t y, int32_t w, int32_t h);
+  void DestroyEvasGL();
+  void DestroyEvasWindow();
+
+  static void RotationEventCb(void* data, Evas_Object* obj, void* event_info);
+  void SendRotationChangeDone();
+
+  Evas_Object* evas_window_{nullptr};
+  Evas_Object* graphics_adapter_{nullptr};
+
+  Evas_GL_Config* gl_config_{nullptr};
+  Evas_GL* evas_gl_{nullptr};
+
+  Evas_GL_Context* gl_context_{nullptr};
+  Evas_GL_Context* gl_resource_context_{nullptr};
+
+  Evas_GL_Surface* gl_surface_{nullptr};
+  Evas_GL_Surface* gl_resource_surface_{nullptr};
+};
+
+#endif  // EMBEDDER_TIZEN_RENDERER_ECORE_WL2_H

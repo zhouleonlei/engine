5c25fbebee6399358695d49a833deb4976cca921
xiaowei guan <60122246+xiaowei-guan@users.noreply.github.com>
2021-05-27 15:17:22 +0800

    Texture api change (#86)
    * Add platfrom surface buffer structor
    
    * Texture api change(draft)
    
    1.Delete tizen texture api, use common api, add GpuBufferTexture for gpu
    buffer.
    
    2.Delete ref/unref tbm_surface code in engine, add destructioncall for
    delete tbm_surface.
    
    3.Support pixel buffer texture.
    
    * Revert code to original
    
    * Support GPU buffer texture
    
    1.Implement GPU buffer texture.
    2.Remove ref/unref tbm surface code.
    
    * Change file permission
    
    * Add FlutterDesktopGpuBufferTextureConfig in FlutterDesktopTextureInfo.
    
    * Add buffer parameter at Destruction callback
    
    * Change class name ExternalTextureGL to ExternalTextureSurfaceGL
    
    Class ExternamTextureSurfaceGL handle tbm surface texture render.
    
    * Remove not used code
    
    * Fix wild pointer issue
    
    If plugin call UnregisterTexture, then will remove external texture,
    But at this time, gpu render is not finish. when gpu render finished,
    tiggle destruction callback, need check whether external texture pointer
    is wild pointer
    
    * Convert unique_ptr to shared_ptr when create external texture
    
    * Code format
    
    * Fix arm64 build error
    
    * Remove not used file
    
    * Remove unnecessary headers
    
    * Refactor based on comments
    
    1.Remove not used file.
    2.Rename define of head file.
    2.Rename of variable.
    
    * Refactor based on comments
    
    1.Return nullptr for default case when create external texture.
    2.Rename methode name from texture_registrar to GetTextureRegistrar.
    3.Change OnCollectTextur to static function.
    
    * Fix code review issue
    
    1.Use C++ style casts
    2.Rename variable and function, make the code more friendly.
    3.Add copyright info.
    
    * Fix code review issue
    
    1.Rename static veriable(nextTextureId -> kNextTextureId)
    2.Modify comment, make it more clear.
    3.Rename function name(FlutterDesktopDestructionCallback)
    
    * Fix code review issue
    
    * Remove warning log
    
    This warning log shouldn't be displayed when launching a headless
    app.

---
+++ shell/platform/tizen/flutter_tizen_texture_registrar.h
@@ -0,0 +1,53 @@
+// Copyright 2020 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EMBEDDER_FLUTTER_TIZEN_TEXTURE_REGISTRAR_H_
+#define EMBEDDER_FLUTTER_TIZEN_TEXTURE_REGISTRAR_H_
+
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+
+#include "flutter/shell/platform/tizen/external_texture.h"
+
+class FlutterTizenEngine;
+
+// An object managing the registration of an external texture.
+// Thread safety: All member methods are thread safe.
+class FlutterTizenTextureRegistrar {
+ public:
+  explicit FlutterTizenTextureRegistrar(FlutterTizenEngine* engine);
+
+  // Registers a texture described by the given |texture_info| object.
+  // Returns the non-zero, positive texture id or -1 on error.
+  int64_t RegisterTexture(const FlutterDesktopTextureInfo* texture_info);
+
+  // Attempts to unregister the texture identified by |texture_id|.
+  // Returns true if the texture was successfully unregistered.
+  bool UnregisterTexture(int64_t texture_id);
+
+  // Notifies the engine about a new frame being available.
+  // Returns true on success.
+  bool MarkTextureFrameAvailable(int64_t texture_id);
+
+  // Attempts to populate the given |texture| by copying the
+  // contents of the texture identified by |texture_id|.
+  // Returns true on success.
+  bool PopulateTexture(int64_t texture_id,
+                       size_t width,
+                       size_t height,
+                       FlutterOpenGLTexture* texture);
+
+  std::unique_ptr<ExternalTexture> CreateExternalTexture(
+      const FlutterDesktopTextureInfo* info);
+
+ private:
+  FlutterTizenEngine* engine_ = nullptr;
+
+  // All registered textures, keyed by their IDs.
+  std::unordered_map<int64_t, std::shared_ptr<ExternalTexture>> textures_;
+  std::mutex map_mutex_;
+};
+
+#endif  // EMBEDDER_FLUTTER_TIZEN_TEXTURE_REGISTRAR_H_

35d809a6b5c976f7ed36521b2213ff6d29bba225
xiaowei guan <60122246+xiaowei-guan@users.noreply.github.com>
2021-05-27 15:17:22 +0800

    Texture api change (#86)
    * Add platfrom surface buffer structor
    
    * Texture api change(draft)
    
    1.Delete tizen texture api, use common api, add GpuBufferTexture for gpu
    buffer.
    
    2.Delete ref/unref tbm_surface code in engine, add destructioncall for
    delete tbm_surface.
    
    3.Support pixel buffer texture.
    
    * Revert code to original
    
    * Support GPU buffer texture
    
    1.Implement GPU buffer texture.
    2.Remove ref/unref tbm surface code.
    
    * Change file permission
    
    * Add FlutterDesktopGpuBufferTextureConfig in FlutterDesktopTextureInfo.
    
    * Add buffer parameter at Destruction callback
    
    * Change class name ExternalTextureGL to ExternalTextureSurfaceGL
    
    Class ExternamTextureSurfaceGL handle tbm surface texture render.
    
    * Remove not used code
    
    * Fix wild pointer issue
    
    If plugin call UnregisterTexture, then will remove external texture,
    But at this time, gpu render is not finish. when gpu render finished,
    tiggle destruction callback, need check whether external texture pointer
    is wild pointer
    
    * Convert unique_ptr to shared_ptr when create external texture
    
    * Code format
    
    * Fix arm64 build error
    
    * Remove not used file
    
    * Remove unnecessary headers
    
    * Refactor based on comments
    
    1.Remove not used file.
    2.Rename define of head file.
    2.Rename of variable.
    
    * Refactor based on comments
    
    1.Return nullptr for default case when create external texture.
    2.Rename methode name from texture_registrar to GetTextureRegistrar.
    3.Change OnCollectTextur to static function.
    
    * Fix code review issue
    
    1.Use C++ style casts
    2.Rename variable and function, make the code more friendly.
    3.Add copyright info.
    
    * Fix code review issue
    
    1.Rename static veriable(nextTextureId -> kNextTextureId)
    2.Modify comment, make it more clear.
    3.Rename function name(FlutterDesktopDestructionCallback)
    
    * Fix code review issue
    
    * Remove warning log
    
    This warning log shouldn't be displayed when launching a headless
    app.

---
+++ shell/platform/tizen/flutter_tizen_texture_registrar.cc
@@ -0,0 +1,108 @@
+// Copyright 2020 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/shell/platform/tizen/flutter_tizen_texture_registrar.h"
+
+#include <iostream>
+#include <mutex>
+
+#include "flutter/shell/platform/tizen/external_texture_pixel_gl.h"
+#include "flutter/shell/platform/tizen/external_texture_surface_gl.h"
+#include "flutter/shell/platform/tizen/flutter_tizen_engine.h"
+#include "flutter/shell/platform/tizen/tizen_log.h"
+
+FlutterTizenTextureRegistrar::FlutterTizenTextureRegistrar(
+    FlutterTizenEngine* engine)
+    : engine_(engine) {}
+
+int64_t FlutterTizenTextureRegistrar::RegisterTexture(
+    const FlutterDesktopTextureInfo* texture_info) {
+  if (texture_info->type != kFlutterDesktopPixelBufferTexture &&
+      texture_info->type != kFlutterDesktopGpuBufferTexture) {
+    FT_LOGE("Attempted to register texture of unsupport type.");
+    return -1;
+  }
+
+  if (texture_info->type == kFlutterDesktopPixelBufferTexture) {
+    if (!texture_info->pixel_buffer_config.callback) {
+      FT_LOGE("Invalid pixel buffer texture callback.");
+      return -1;
+    }
+  }
+
+  if (texture_info->type == kFlutterDesktopGpuBufferTexture) {
+    if (!texture_info->gpu_buffer_config.callback) {
+      FT_LOGE("Invalid gpu buffer texture callback.");
+      return -1;
+    }
+  }
+  auto texture_gl = CreateExternalTexture(texture_info);
+  int64_t texture_id = texture_gl->TextureId();
+
+  {
+    std::lock_guard<std::mutex> lock(map_mutex_);
+    textures_[texture_id] = std::move(texture_gl);
+  }
+
+  FlutterEngineRegisterExternalTexture(engine_->flutter_engine, texture_id);
+  return texture_id;
+}
+
+bool FlutterTizenTextureRegistrar::UnregisterTexture(int64_t texture_id) {
+  {
+    std::lock_guard<std::mutex> lock(map_mutex_);
+    auto it = textures_.find(texture_id);
+    if (it == textures_.end()) {
+      return false;
+    }
+    textures_.erase(it);
+  }
+
+  return FlutterEngineUnregisterExternalTexture(engine_->flutter_engine,
+                                                texture_id) == kSuccess;
+}
+
+bool FlutterTizenTextureRegistrar::MarkTextureFrameAvailable(
+    int64_t texture_id) {
+  return FlutterEngineMarkExternalTextureFrameAvailable(engine_->flutter_engine,
+                                                        texture_id) == kSuccess;
+}
+
+bool FlutterTizenTextureRegistrar::PopulateTexture(
+    int64_t texture_id,
+    size_t width,
+    size_t height,
+    FlutterOpenGLTexture* opengl_texture) {
+  ExternalTexture* texture;
+  {
+    std::lock_guard<std::mutex> lock(map_mutex_);
+    auto it = textures_.find(texture_id);
+    if (it == textures_.end()) {
+      return false;
+    }
+    texture = it->second.get();
+  }
+  return texture->PopulateTexture(width, height, opengl_texture);
+}
+
+std::unique_ptr<ExternalTexture>
+FlutterTizenTextureRegistrar::CreateExternalTexture(
+    const FlutterDesktopTextureInfo* texture_info) {
+  switch (texture_info->type) {
+    case kFlutterDesktopPixelBufferTexture:
+      return std::make_unique<ExternalTexturePixelGL>(
+          texture_info->pixel_buffer_config.callback,
+          texture_info->pixel_buffer_config.user_data);
+      break;
+    case kFlutterDesktopGpuBufferTexture:
+      return std::make_unique<ExternalTextureSurfaceGL>(
+          texture_info->gpu_buffer_config.callback,
+          texture_info->gpu_buffer_config.destruction_callback,
+          texture_info->gpu_buffer_config.user_data);
+      break;
+    default:
+      FT_LOGE("Invalid texture type.");
+      return nullptr;
+  }
+}

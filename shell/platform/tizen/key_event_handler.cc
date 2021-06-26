8592c9ff0fa482a7b6f2a8078bab9b5e70cad4d8
Swift Kim <swift.kim@samsung.com>
2021-06-26 09:18:23 +0900

    Let the app handle the back key event first (#126)
    * Let the app handle the back key event first
    
    * Tidy up

--- shell/platform/tizen/key_event_handler.cc
+++ shell/platform/tizen/key_event_handler.cc
@@ -4,11 +4,19 @@
 
 #include "key_event_handler.h"
 
-#include "flutter/shell/platform/tizen/flutter_tizen_engine.h"
+#include <app.h>
 
-static constexpr char kPlatformBackButtonName[] = "XF86Back";
+#include "flutter/shell/platform/tizen/flutter_tizen_engine.h"
+#include "flutter/shell/platform/tizen/tizen_log.h"
 
 namespace flutter {
+
+namespace {
+
+constexpr char kBackKey[] = "XF86Back";
+constexpr char kExitKey[] = "XF86Exit";
+
+}  // namespace
 
 KeyEventHandler::KeyEventHandler(FlutterTizenEngine* engine) : engine_(engine) {
   key_event_handlers_.push_back(
@@ -30,23 +38,33 @@ Eina_Bool KeyEventHandler::OnKey(void* data, int type, void* event) {
   auto* engine = self->engine_;
   auto is_down = type == ECORE_EVENT_KEY_DOWN;
 
-  if (strcmp(key->keyname, kPlatformBackButtonName) == 0) {
-    // The device back button was pressed.
-    if (engine->navigation_channel && !is_down) {
-      engine->navigation_channel->PopRoute();
+  FT_LOGI("Keycode: %d, name: %s, mods: %d, is_down: %d", key->keycode,
+          key->keyname, key->modifiers, is_down);
+
+  if (engine->text_input_channel) {
+    if (is_down) {
+      engine->text_input_channel->OnKeyDown(key);
     }
-  } else {
-    if (engine->text_input_channel) {
-      if (is_down) {
-        engine->text_input_channel->OnKeyDown(key);
-      }
-      if (engine->text_input_channel->IsSoftwareKeyboardShowing()) {
-        return ECORE_CALLBACK_PASS_ON;
-      }
+    if (engine->text_input_channel->IsSoftwareKeyboardShowing()) {
+      return ECORE_CALLBACK_PASS_ON;
     }
-    if (engine->key_event_channel) {
-      engine->key_event_channel->SendKeyEvent(key, is_down);
-    }
+  }
+
+  if (engine->key_event_channel) {
+    engine->key_event_channel->SendKeyEvent(
+        key, is_down,
+        [engine, keyname = std::string(key->keyname), is_down](bool handled) {
+          if (handled) {
+            return;
+          }
+          if (keyname == kBackKey && !is_down) {
+            if (engine->navigation_channel) {
+              engine->navigation_channel->PopRoute();
+            }
+          } else if (keyname == kExitKey && !is_down) {
+            ui_app_exit();
+          }
+        });
   }
   return ECORE_CALLBACK_PASS_ON;
 }

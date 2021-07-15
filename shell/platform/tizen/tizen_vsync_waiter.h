ee03ccc9aff6dc7e0d45ddb3a11de6c89bb483eb
xiaowei guan <60122246+xiaowei-guan@users.noreply.github.com>
2021-07-15 22:32:37 +0800

    Fix vsync not support engine group issue (#145)
    * Fix vsync waiter not support engine group issue
    
    * Rename some methods name
    
    * Make engine_ private and rename TdmValid to IsValid

--- shell/platform/tizen/tizen_vsync_waiter.h
+++ shell/platform/tizen/tizen_vsync_waiter.h
@@ -14,6 +14,29 @@ namespace flutter {
 
 class FlutterTizenEngine;
 
+class TdmClient {
+ public:
+  TdmClient(FlutterTizenEngine* engine);
+  virtual ~TdmClient();
+  bool CreateTdm();
+  void DestroyTdm();
+  bool IsValid();
+  void WaitVblank(intptr_t baton);
+  static void VblankCallback(tdm_client_vblank* vblank,
+                             tdm_error error,
+                             unsigned int sequence,
+                             unsigned int tv_sec,
+                             unsigned int tv_usec,
+                             void* user_data);
+
+ private:
+  tdm_client* client_{nullptr};
+  tdm_client_output* output_{nullptr};
+  tdm_client_vblank* vblank_{nullptr};
+  FlutterTizenEngine* engine_{nullptr};
+  intptr_t baton_{0};
+};
+
 class TizenVsyncWaiter {
  public:
   TizenVsyncWaiter(FlutterTizenEngine* engine);
@@ -21,24 +44,11 @@ class TizenVsyncWaiter {
   void AsyncWaitForVsync(intptr_t baton);
 
  private:
-  bool CreateTDMVblank();
-  void DestroyTDMVblank();
-  bool TDMValid();
-  void SendMessage(int val);
-  static void TdmClientVblankCallback(tdm_client_vblank* vblank,
-                                      tdm_error error,
-                                      unsigned int sequence,
-                                      unsigned int tv_sec,
-                                      unsigned int tv_usec,
-                                      void* user_data);
+  void Send(int event, intptr_t baton);
   static void RequestVblankLoop(void* data, Ecore_Thread* thread);
-  static void VblankLoopFinish(void* data, Ecore_Thread* thread);
-  tdm_client* client_{nullptr};
-  tdm_client_output* output_{nullptr};
-  tdm_client_vblank* vblank_{nullptr};
-  FlutterTizenEngine* engine_{nullptr};
-  intptr_t baton_{0};
   Ecore_Thread* vblank_thread_{nullptr};
+  Eina_Thread_Queue* vblank_thread_queue_{nullptr};
+  FlutterTizenEngine* engine_{nullptr};
 };
 
 }  // namespace flutter

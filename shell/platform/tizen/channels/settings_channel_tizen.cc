d57107692453aa0212fbd6ff89fb30aa72e5414a
MuHong Byun <bwikbs@gmail.com>
2021-07-19 14:50:21 +0900

    Unit tests for the settings channel (#146)
    * Add setting channel unit test
    
    * Update minor things:
    
    * Update flutter_tizen_engine_unittest (localization)
    * Rename file :
      settings_channel_device.cc -> settings_channel_tizen.cc
      settings_channel_stub.cc -> settings_channel_linux.cc
    
    Signed-off-by: MuHong Byun <mh.byun@samsung.com>

---
+++ shell/platform/tizen/channels/settings_channel_tizen.cc
@@ -0,0 +1,36 @@
+// Copyright 2021 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <system/system_settings.h>
+#include "settings_channel.h"
+
+namespace flutter {
+
+static void OnSettingsChangedCallback(system_settings_key_e key,
+                                      void* user_data) {
+  auto settings_channel = reinterpret_cast<SettingsChannel*>(user_data);
+  settings_channel->SendSettingsEvent();
+}
+
+void SettingsChannel::Init() {
+  system_settings_set_changed_cb(SYSTEM_SETTINGS_KEY_LOCALE_TIMEFORMAT_24HOUR,
+                                 OnSettingsChangedCallback, this);
+}
+
+void SettingsChannel::Dispose() {
+  system_settings_unset_changed_cb(
+      SYSTEM_SETTINGS_KEY_LOCALE_TIMEFORMAT_24HOUR);
+}
+
+bool SettingsChannel::Prefer24HourTime() {
+  bool value = false;
+  if (system_settings_get_value_bool(
+          SYSTEM_SETTINGS_KEY_LOCALE_TIMEFORMAT_24HOUR, &value) ==
+      SYSTEM_SETTINGS_ERROR_NONE) {
+    return value;
+  }
+  return false;
+}
+
+}  // namespace flutter

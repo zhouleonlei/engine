03a6426f26857ff7cdb676fac0320a8c57eebe98
Swift Kim <swift.kim@samsung.com>
2021-10-27 09:48:38 +0900

    Clean up the text input-related code (#198)
    * Replace pannel with panel
    
    * Refactor tizen_input_method_context
    
    - Remove redundant vertical whitespaces.
    - Change std::string to const std::string&.
    - Remove Init and Deinit.
    - Minor formatting.
    
    * Use ECORE_IMF_INPUT_PANEL_LAYOUT_NORMAL by default
    
    * Use plurals
    
    * Refactor text_input_channel
    
    - Remove unnecessary include.
    - Replace iterator suffix _json with _iter.
    - Minor formatting.

--- shell/platform/tizen/tizen_input_method_context.cc
+++ shell/platform/tizen/tizen_input_method_context.cc
@@ -9,7 +9,7 @@
 
 namespace {
 
-const char* GetEcoreImfContextAvailableID() {
+const char* GetEcoreImfContextAvailableId() {
   Eina_List* modules;
 
   modules = ecore_imf_context_available_ids_get();
@@ -23,81 +23,66 @@ const char* GetEcoreImfContextAvailableID() {
   return nullptr;
 }
 
-bool TextInputTypeToEcoreIMFInputPanelLayout(
-    std::string text_input_type,
-    Ecore_IMF_Input_Panel_Layout* panel_layout) {
+Ecore_IMF_Input_Panel_Layout TextInputTypeToEcoreIMFInputPanelLayout(
+    const std::string& text_input_type) {
   FT_ASSERT(panel_layout);
   if (text_input_type == "TextInputType.text" ||
       text_input_type == "TextInputType.multiline") {
-    *panel_layout = ECORE_IMF_INPUT_PANEL_LAYOUT_NORMAL;
+    return ECORE_IMF_INPUT_PANEL_LAYOUT_NORMAL;
   } else if (text_input_type == "TextInputType.number") {
-    *panel_layout = ECORE_IMF_INPUT_PANEL_LAYOUT_NUMBERONLY;
+    return ECORE_IMF_INPUT_PANEL_LAYOUT_NUMBERONLY;
   } else if (text_input_type == "TextInputType.phone") {
-    *panel_layout = ECORE_IMF_INPUT_PANEL_LAYOUT_PHONENUMBER;
+    return ECORE_IMF_INPUT_PANEL_LAYOUT_PHONENUMBER;
   } else if (text_input_type == "TextInputType.datetime") {
-    *panel_layout = ECORE_IMF_INPUT_PANEL_LAYOUT_DATETIME;
+    return ECORE_IMF_INPUT_PANEL_LAYOUT_DATETIME;
   } else if (text_input_type == "TextInputType.emailAddress") {
-    *panel_layout = ECORE_IMF_INPUT_PANEL_LAYOUT_EMAIL;
+    return ECORE_IMF_INPUT_PANEL_LAYOUT_EMAIL;
   } else if (text_input_type == "TextInputType.url") {
-    *panel_layout = ECORE_IMF_INPUT_PANEL_LAYOUT_URL;
+    return ECORE_IMF_INPUT_PANEL_LAYOUT_URL;
   } else if (text_input_type == "TextInputType.visiblePassword") {
-    *panel_layout = ECORE_IMF_INPUT_PANEL_LAYOUT_PASSWORD;
-  } else if (text_input_type == "TextInputType.name" ||
-             text_input_type == "TextInputType.address") {
+    return ECORE_IMF_INPUT_PANEL_LAYOUT_PASSWORD;
+  } else {
     FT_LOG(Warn) << "The requested input type " << text_input_type
                  << " is not supported.";
-    *panel_layout = ECORE_IMF_INPUT_PANEL_LAYOUT_NORMAL;
-  } else {
-    return false;
+    return ECORE_IMF_INPUT_PANEL_LAYOUT_NORMAL;
   }
-  return true;
 }
 
-Ecore_IMF_Keyboard_Modifiers EcoreInputModifierToEcoreIMFModifier(
-    unsigned int ecoreModifier) {
-  unsigned int modifier(ECORE_IMF_KEYBOARD_MODIFIER_NONE);
-
-  if (ecoreModifier & ECORE_EVENT_MODIFIER_SHIFT) {
-    modifier |= ECORE_IMF_KEYBOARD_MODIFIER_SHIFT;
+Ecore_IMF_Keyboard_Modifiers EcoreInputModifiersToEcoreIMFModifiers(
+    unsigned int ecore_modifiers) {
+  unsigned int modifiers(ECORE_IMF_KEYBOARD_MODIFIER_NONE);
+  if (ecore_modifiers & ECORE_EVENT_MODIFIER_SHIFT) {
+    modifiers |= ECORE_IMF_KEYBOARD_MODIFIER_SHIFT;
   }
-
-  if (ecoreModifier & ECORE_EVENT_MODIFIER_ALT) {
-    modifier |= ECORE_IMF_KEYBOARD_MODIFIER_ALT;
+  if (ecore_modifiers & ECORE_EVENT_MODIFIER_ALT) {
+    modifiers |= ECORE_IMF_KEYBOARD_MODIFIER_ALT;
   }
-
-  if (ecoreModifier & ECORE_EVENT_MODIFIER_CTRL) {
-    modifier |= ECORE_IMF_KEYBOARD_MODIFIER_CTRL;
+  if (ecore_modifiers & ECORE_EVENT_MODIFIER_CTRL) {
+    modifiers |= ECORE_IMF_KEYBOARD_MODIFIER_CTRL;
   }
-
-  if (ecoreModifier & ECORE_EVENT_MODIFIER_WIN) {
-    modifier |= ECORE_IMF_KEYBOARD_MODIFIER_WIN;
+  if (ecore_modifiers & ECORE_EVENT_MODIFIER_WIN) {
+    modifiers |= ECORE_IMF_KEYBOARD_MODIFIER_WIN;
   }
-
-  if (ecoreModifier & ECORE_EVENT_MODIFIER_ALTGR) {
-    modifier |= ECORE_IMF_KEYBOARD_MODIFIER_ALTGR;
+  if (ecore_modifiers & ECORE_EVENT_MODIFIER_ALTGR) {
+    modifiers |= ECORE_IMF_KEYBOARD_MODIFIER_ALTGR;
   }
-
-  return static_cast<Ecore_IMF_Keyboard_Modifiers>(modifier);
+  return static_cast<Ecore_IMF_Keyboard_Modifiers>(modifiers);
 }
 
-Ecore_IMF_Keyboard_Locks EcoreInputModifierToEcoreIMFLock(
-    unsigned int modifier) {
+Ecore_IMF_Keyboard_Locks EcoreInputModifiersToEcoreIMFLocks(
+    unsigned int modifiers) {
   // If no other matches, returns NONE.
-  unsigned int lock(ECORE_IMF_KEYBOARD_LOCK_NONE);
-
-  if (modifier & ECORE_EVENT_LOCK_NUM) {
-    lock |= ECORE_IMF_KEYBOARD_LOCK_NUM;
+  unsigned int locks(ECORE_IMF_KEYBOARD_LOCK_NONE);
+  if (modifiers & ECORE_EVENT_LOCK_NUM) {
+    locks |= ECORE_IMF_KEYBOARD_LOCK_NUM;
   }
-
-  if (modifier & ECORE_EVENT_LOCK_CAPS) {
-    lock |= ECORE_IMF_KEYBOARD_LOCK_CAPS;
+  if (modifiers & ECORE_EVENT_LOCK_CAPS) {
+    locks |= ECORE_IMF_KEYBOARD_LOCK_CAPS;
   }
-
-  if (modifier & ECORE_EVENT_LOCK_SCROLL) {
-    lock |= ECORE_IMF_KEYBOARD_LOCK_SCROLL;
+  if (modifiers & ECORE_EVENT_LOCK_SCROLL) {
+    locks |= ECORE_IMF_KEYBOARD_LOCK_SCROLL;
   }
-
-  return static_cast<Ecore_IMF_Keyboard_Locks>(lock);
+  return static_cast<Ecore_IMF_Keyboard_Locks>(locks);
 }
 
 }  // namespace
@@ -107,21 +92,12 @@ namespace flutter {
 TizenInputMethodContext::TizenInputMethodContext(FlutterTizenEngine* engine)
     : engine_(engine) {
   FT_ASSERT(engine_);
-  Init();
-}
-
-TizenInputMethodContext::~TizenInputMethodContext() {
-  Deinit();
-}
-
-void TizenInputMethodContext::Init() {
-  FT_ASSERT(engine_);
   ecore_imf_init();
 
   const char* imf_id = ecore_imf_context_default_id_get();
   if (imf_id == nullptr) {
     // Try to get a fallback ID.
-    imf_id = GetEcoreImfContextAvailableID();
+    imf_id = GetEcoreImfContextAvailableId();
   }
   if (imf_id == nullptr) {
     FT_LOG(Error) << "Failed to get an IMF context ID.";
@@ -138,11 +114,11 @@ void TizenInputMethodContext::Init() {
       imf_context_,
       reinterpret_cast<void*>(engine_->renderer()->GetWindowId()));
   SetContextOptions();
-  SetInputPannelOptions();
+  SetInputPanelOptions();
   RegisterEventCallbacks();
 }
 
-void TizenInputMethodContext::Deinit() {
+TizenInputMethodContext::~TizenInputMethodContext() {
   UnregisterEventCallbacks();
 
   if (imf_context_) {
@@ -164,21 +140,20 @@ bool TizenInputMethodContext::FilterEvent(Ecore_Event_Key* event,
   imf_event.string = event->string;
   imf_event.compose = event->compose;
   imf_event.timestamp = event->timestamp;
-  imf_event.modifiers = EcoreInputModifierToEcoreIMFModifier(event->modifiers);
-  imf_event.locks = EcoreInputModifierToEcoreIMFLock(event->modifiers);
+  imf_event.modifiers =
+      EcoreInputModifiersToEcoreIMFModifiers(event->modifiers);
+  imf_event.locks = EcoreInputModifiersToEcoreIMFLocks(event->modifiers);
   imf_event.dev_name = dev_name;
   imf_event.keycode = event->keycode;
 
-  bool ret = ecore_imf_context_filter_event(
+  return ecore_imf_context_filter_event(
       imf_context_, ECORE_IMF_EVENT_KEY_DOWN,
       reinterpret_cast<Ecore_IMF_Event*>(&imf_event));
-
-  return ret;
 }
 
-InputPannelGeometry TizenInputMethodContext::GetInputPannelGeometry() {
+InputPanelGeometry TizenInputMethodContext::GetInputPanelGeometry() {
   FT_ASSERT(imf_context_);
-  InputPannelGeometry geometry;
+  InputPanelGeometry geometry;
   ecore_imf_context_input_panel_geometry_get(
       imf_context_, &geometry.x, &geometry.y, &geometry.w, &geometry.h);
   return geometry;
@@ -189,24 +164,23 @@ void TizenInputMethodContext::ResetInputMethodContext() {
   ecore_imf_context_reset(imf_context_);
 }
 
-void TizenInputMethodContext::ShowInputPannel() {
+void TizenInputMethodContext::ShowInputPanel() {
   FT_ASSERT(imf_context_);
   ecore_imf_context_input_panel_show(imf_context_);
   ecore_imf_context_focus_in(imf_context_);
 }
 
-void TizenInputMethodContext::HideInputPannel() {
+void TizenInputMethodContext::HideInputPanel() {
   FT_ASSERT(imf_context_);
   ecore_imf_context_focus_out(imf_context_);
   ecore_imf_context_input_panel_hide(imf_context_);
 }
 
-void TizenInputMethodContext::SetInputPannelLayout(std::string input_type) {
+void TizenInputMethodContext::SetInputPanelLayout(
+    const std::string& input_type) {
   FT_ASSERT(imf_context_);
-  Ecore_IMF_Input_Panel_Layout panel_layout;
-  if (TextInputTypeToEcoreIMFInputPanelLayout(input_type, &panel_layout)) {
-    ecore_imf_context_input_panel_layout_set(imf_context_, panel_layout);
-  }
+  auto panel_layout = TextInputTypeToEcoreIMFInputPanelLayout(input_type);
+  ecore_imf_context_input_panel_layout_set(imf_context_, panel_layout);
 }
 
 void TizenInputMethodContext::SetInputPanelLayoutVariation(bool is_signed,
@@ -293,8 +267,8 @@ void TizenInputMethodContext::RegisterEventCallbacks() {
       [](void* data, Ecore_IMF_Context* context, int value) {
         flutter::TizenInputMethodContext* self =
             static_cast<flutter::TizenInputMethodContext*>(data);
-        if (self->on_input_pannel_state_changed_) {
-          self->on_input_pannel_state_changed_(value);
+        if (self->on_input_panel_state_changed_) {
+          self->on_input_panel_state_changed_(value);
         }
       },
       this);
@@ -324,7 +298,7 @@ void TizenInputMethodContext::SetContextOptions() {
   ecore_imf_context_prediction_allow_set(imf_context_, EINA_FALSE);
 }
 
-void TizenInputMethodContext::SetInputPannelOptions() {
+void TizenInputMethodContext::SetInputPanelOptions() {
   FT_ASSERT(imf_context_);
   ecore_imf_context_input_panel_layout_set(imf_context_,
                                            ECORE_IMF_INPUT_PANEL_LAYOUT_NORMAL);

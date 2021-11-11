ffac44778a56259f1ccd710a432c11303293eddf
Swift Kim <swift.kim@samsung.com>
2021-11-11 13:28:50 +0900

    Implement the forwarding logger (#203)
    * Initial implementation
    
    * Change switches to engine_arguments
    
    * Do not log debug messages
    
    * Fix x64 build

--- shell/platform/tizen/logger.cc
+++ shell/platform/tizen/logger.cc
@@ -7,88 +7,191 @@
 #ifndef __X64_SHELL__
 #include <dlog.h>
 #endif
+#include <netinet/in.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
+#include <cerrno>
 #include <cstdlib>
 #include <iostream>
+
+namespace {
+
+#ifndef __X64_SHELL__
+constexpr char kLogTag[] = "ConsoleMessage";
+
+std::string GetLevelName(int level) {
+  switch (level) {
+    case flutter::kLogLevelDebug:
+      return "D";
+    default:
+      return "I";
+    case flutter::kLogLevelWarn:
+      return "W";
+    case flutter::kLogLevelError:
+      return "E";
+    case flutter::kLogLevelFatal:
+      return "F";
+  }
+}
+
+log_priority LevelToPriority(int level) {
+  switch (level) {
+    case flutter::kLogLevelDebug:
+      return DLOG_DEBUG;
+    default:
+      return DLOG_INFO;
+    case flutter::kLogLevelWarn:
+      return DLOG_WARN;
+    case flutter::kLogLevelError:
+      return DLOG_ERROR;
+    case flutter::kLogLevelFatal:
+      return DLOG_FATAL;
+  }
+}
+#endif  // __X64_SHELL__
+
+}  // namespace
 
 namespace flutter {
 
 void* Logger::Redirect(void* arg) {
   int* pipe = static_cast<int*>(arg);
   ssize_t size;
-  char buffer[1024];
+  char buffer[4096];
 
-  while ((size = read(pipe[0], buffer, sizeof(buffer) - 1)) > 0) {
+  while ((size = read(pipe[0], buffer, sizeof(buffer) - 1)) > 0 &&
+         is_running_) {
     buffer[size] = 0;
     Print(pipe == stdout_pipe_ ? kLogLevelInfo : kLogLevelError,
           std::string(buffer));
   }
+  return nullptr;
+}
 
-  close(pipe[0]);
-  close(pipe[1]);
+void* Logger::Forward(void* arg) {
+  if (logging_port_ == 0) {
+    return nullptr;
+  }
 
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd < 0) {
+    FT_LOG(Error) << "Error opening a socket.";
+    return nullptr;
+  }
+  int optval = 1;
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(logging_port_);
+  if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    FT_LOG(Error) << "Error on binding: " << strerror(errno);
+    close(fd);
+    return nullptr;
+  }
+  if (listen(fd, 1) < 0) {
+    FT_LOG(Error) << "Error listening to incoming connection: "
+                  << strerror(errno);
+    close(fd);
+    return nullptr;
+  }
+
+  struct sockaddr_in client_addr;
+  socklen_t addr_len = sizeof(client_addr);
+  int new_fd = accept(fd, (struct sockaddr*)&client_addr, &addr_len);
+  if (new_fd < 0) {
+    FT_LOG(Error) << "Error on accept: " << strerror(errno);
+    close(fd);
+    return nullptr;
+  }
+  FT_LOG(Info) << "Connection accepted on port: " << logging_port_;
+  close(fd);
+
+  ssize_t size;
+  char buffer[4096];
+
+  if (write(new_fd, "ACCEPTED", 8) < 0) {
+    FT_LOG(Error) << "Error writing to socket: " << strerror(errno);
+    close(new_fd);
+    return nullptr;
+  }
+  while ((size = read(logging_pipe_[0], buffer, sizeof(buffer))) > 0 &&
+         is_running_) {
+    if (write(new_fd, buffer, size) < 0) {
+      FT_LOG(Error) << "Error writing to socket: " << strerror(errno);
+      break;
+    }
+  }
+  close(new_fd);
   return nullptr;
 }
 
 void Logger::Start() {
-  if (started_) {
+  if (is_running_) {
     FT_LOG(Info) << "The threads have already started.";
     return;
   }
-  if (pipe(stdout_pipe_) < 0 || pipe(stderr_pipe_) < 0) {
+  if (pipe(stdout_pipe_) < 0 || pipe(stderr_pipe_) < 0 ||
+      pipe(logging_pipe_) < 0) {
     FT_LOG(Error) << "Failed to create pipes.";
     return;
   }
+  is_running_ = true;
+
   if (dup2(stdout_pipe_[1], 1) < 0 || dup2(stderr_pipe_[1], 2) < 0) {
     FT_LOG(Error) << "Failed to duplicate file descriptors.";
     return;
   }
   if (pthread_create(&stdout_thread_, 0, Redirect, stdout_pipe_) != 0 ||
-      pthread_create(&stderr_thread_, 0, Redirect, stderr_pipe_) != 0) {
+      pthread_create(&stderr_thread_, 0, Redirect, stderr_pipe_) != 0 ||
+      pthread_create(&logging_thread_, 0, Forward, nullptr) != 0) {
     FT_LOG(Error) << "Failed to create threads.";
     return;
   }
   if (pthread_detach(stdout_thread_) != 0 ||
-      pthread_detach(stderr_thread_) != 0) {
+      pthread_detach(stderr_thread_) != 0 ||
+      pthread_detach(logging_thread_) != 0) {
     FT_LOG(Warn) << "Failed to detach threads.";
   }
-  started_ = true;
 }
 
-int Logger::GetLoggingLevel() {
-  return logging_level_;
+void Logger::Stop() {
+  if (!is_running_) {
+    return;
+  }
+  is_running_ = false;
+
+  close(stdout_pipe_[0]);
+  close(stdout_pipe_[1]);
+  close(stderr_pipe_[0]);
+  close(stderr_pipe_[1]);
+  close(logging_pipe_[0]);
+  close(logging_pipe_[1]);
 }
 
-void Logger::SetLoggingLevel(int level) {
-  logging_level_ = level;
-}
-
-void Logger::Print(int level, std::string message) {
+void Logger::Print(int level, const std::string& message) {
 #ifdef __X64_SHELL__
   std::cerr << message << std::endl;
   std::cerr.flush();
 #else
-  log_priority priority;
-  if (level == kLogLevelDebug) {
-    priority = DLOG_DEBUG;
-  } else if (level == kLogLevelInfo) {
-    priority = DLOG_INFO;
-  } else if (level == kLogLevelWarn) {
-    priority = DLOG_WARN;
-  } else if (level == kLogLevelError) {
-    priority = DLOG_ERROR;
-  } else if (level == kLogLevelFatal) {
-    priority = DLOG_FATAL;
-  } else {
-    priority = DLOG_INFO;
+  // Write the message to the logging pipe so that it can be forwarded to the
+  // connected host.
+  if (logging_port_ > 0 && level >= kLogLevelInfo) {
+    std::string prefix = "[" + GetLevelName(level) + "] ";
+    std::string formatted = prefix + message + "\n";
+    write(logging_pipe_[1], formatted.c_str(), formatted.size());
   }
+
+  // Also print to dlog for convenience.
+  log_priority priority = LevelToPriority(level);
 #ifdef TV_PROFILE
-  // LOG_ID_MAIN must be used to display logs properly on TV devices.
-  // Note: dlog_print(...) is an alias of __dlog_print(LOG_ID_APPS, ...).
-  __dlog_print(LOG_ID_MAIN, priority, "ConsoleMessage", "%s", message.c_str());
+  // dlog_print(..) which is an alias of __dlog_print(LOG_ID_APPS, ..) is not
+  // valid on TV devices.
+  __dlog_print(LOG_ID_MAIN, priority, kLogTag, "%s", message.c_str());
 #else
-  dlog_print(priority, "ConsoleMessage", "%s", message.c_str());
+  dlog_print(priority, kLogTag, "%s", message.c_str());
 #endif
 #endif  // __X64_SHELL__
 }
